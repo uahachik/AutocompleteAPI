4aab6cf505bb45c4c36f0fbb8d81363c3e0f7658
Roman <kotenkoroman2019@gmail.com>
2019-04-25 09:32:38 +0300

    initial commit

---
+++ autocomplete.js
@@ -0,0 +1,40 @@
+const url = 'main.php';
+const command = 'auto_complete_multi_location';
+
+$(document).ready(function () {
+    function updateTextBox(event, ui) {
+        $(this).val(ui.item.name);
+        return false;
+    }
+
+    $('#sugg').autocomplete({
+        // minLength: 1,
+        delay: 500,
+        focus: updateTextBox,
+        select: updateTextBox,
+        source: function (request, response){
+            $.ajax({
+                url,
+                method: 'POST',
+                dataType: 'json',
+                data: {keyword: request.term, command},
+                success: (function (data) {
+                    response(data);
+                })
+            });
+        }
+    })
+        .autocomplete('instance')._renderItem = function (ul, item) {
+        let output;
+        if (item.type == 0) {
+            output = `<li><i class="fas fa-plane"></i><span>${item.name}</span></li>`
+        } else if (item.type == 1) {
+            output = `<li><i class="fas fa-map-marker-alt"></i><span>${item.name}</span></li>`
+        } else {
+            output = `<li><i class="fas fa-ship"></i><span>${item.name}</span></li>`
+        }
+        return $('<li id="color">')
+            .append(output)
+            .appendTo(ul);
+    }
+});
